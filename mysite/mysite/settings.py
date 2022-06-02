97205919fa2ba7411a7ab8e82ded4b89127d95a5
ozzigit <ozzi@ukr.net>
2022-06-02 13:19:54 +0300

    env from idea

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c($raems7qp-v+t*-f@#r#nz!d3946%@if)i3sp6*4e=pwptj4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = False
 DEBUG = True
 
 ALLOWED_HOSTS = []
